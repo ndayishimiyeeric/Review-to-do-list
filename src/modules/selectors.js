ca424ef880f114a36eb773aa5bf1b2208f42ec08
ndayishimiyeeric <ndayishimiyeeric86@gmail.com>
2022-05-03 15:47:43 +0200

    Add selectors module

---
+++ src/modules/selectors.js
@@ -0,0 +1,10 @@
+const addForm = document.querySelector('#add-task');
+const reset = document.querySelector('.reflesh');
+const clearAll = document.querySelector('.clear-completed');
+
+const taskDiv = document.querySelector('.todo-tasks');
+const deleteBtn = document.querySelectorAll('.delete');
+
+export {
+  addForm, reset, clearAll, taskDiv, deleteBtn,
+};
