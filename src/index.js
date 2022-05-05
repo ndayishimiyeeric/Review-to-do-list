7618aded6caf6e5691a248a99cd6f20aa1ae0c51
ndayishimiyeeric <ndayishimiyeeric86@gmail.com>
2022-05-05 17:56:40 +0200

    Initialize jest

--- src/index.js
+++ src/index.js
@@ -1,32 +1,42 @@
 import './styles/main.scss';
-import Tasks from './modules/ClassStore.js';
+// import Tasks, { tasksArray } from './modules/ClassStore.js';
+// import { tasksArray } from './modules/ClassStore.js';
 import {
   addForm, reset, clearAll,
 } from './modules/selectors.js';
 
-const tasksCollection = new Tasks();
-tasksCollection.displayTasks();
+import {
+  displayTasks, addTask, tasksArray,
+} from './modules/ClassStore.js';
+
+// const tasksCollection = new Tasks();
+// tasksCollection.displayTasks();
+displayTasks();
 
 const setLocalStorage = () => {
-  localStorage.setItem('tasks', JSON.stringify(tasksCollection.tasksArray));
+  localStorage.setItem('tasks', JSON.stringify(tasksArray));
 };
 
 addForm.addEventListener('submit', (e) => {
   e.preventDefault();
   const inputTaskValue = document.querySelector('#input-add-task').value;
   if (inputTaskValue === null || inputTaskValue === '') return;
-  tasksCollection.addTask(inputTaskValue);
+  // tasksCollection.addTask(inputTaskValue);
+  addTask(inputTaskValue);
   document.querySelector('#input-add-task').value = '';
 });
 
 reset.addEventListener('click', () => {
-  tasksCollection.tasksArray = [];
+  tasksArray = [];
   setLocalStorage();
-  tasksCollection.displayTasks();
+  // tasksCollection.displayTasks();
+  displayTasks();
 });
 
 clearAll.addEventListener('click', () => {
-  tasksCollection.tasksArray = tasksCollection.tasksArray.filter((task) => !task.complete);
-  setLocalStorage();
-  tasksCollection.displayTasks();
+  const newtasksArray = tasksArray.filter((task) => !task.complete);
+  localStorage.setItem('tasks', JSON.stringify(newtasksArray));
+  // tasksCollection.displayTasks();
+  displayTasks();
+  document.location.reload();
 });
