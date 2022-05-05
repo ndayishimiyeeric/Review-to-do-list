55856d1bbe1491571b8fae56567569ff39f092d4
talha <tkz9696@gmail.com>
2022-05-05 21:29:13 +0500

    Write tests for add and delete

--- src/test/ClassStore.test.js
+++ src/test/ClassStore.test.js
@@ -7,4 +7,23 @@ describe('Add and Delete Function', () => {
     clear: jest.fn(),
   };
   global.localStorage = localStorageMock;
+  test('add item to array', () => {
+    addTask('Play');
+    const expected = {
+      description: 'Play',
+      index: 1,
+      complete: false,
+    };
+    expect(tasksArray).toEqual([expected]);
+    expect(tasksArray.length).toBe(1);
+    expect(global.localStorage.setItem).toBeCalledWith('tasks', JSON.stringify(tasksArray));
+  });
+
+  test('remove item from array', () => {
+    const array = [];
+    deleteItem(array, 1);
+    expect(array).toEqual([]);
+    expect(array.length).toBe(0);
+    expect(global.localStorage.setItem).toBeCalledWith('tasks', JSON.stringify(tasksArray));
+  });
 });
