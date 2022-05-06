33c3d735c608b6dd2eacca9201bca4857d1aeb8a
Talha <tkz9696@gmail.com>
2022-05-06 18:35:22 +0500

    Add Clear Function Test + Linter errors

--- src/test/ClassStore.test.js
+++ src/test/ClassStore.test.js
@@ -25,3 +25,44 @@ describe('Add and Delete Function', () => {
     expect(global.localStorage.setItem).toBeCalledWith('tasks', JSON.stringify(tasksArray));
   });
 });
+
+describe('delete completed tasks, Update a task to completed, and edit a task description', () => {
+  test('delete completed tasks', () => {
+    const localStorageMock = {
+      getItem: jest.fn(),
+      setItem: jest.fn(),
+      clear: jest.fn(),
+    };
+    global.localStorage = localStorageMock;
+    global.localStorage.setItem('tasks', JSON.stringify([
+      { description: 'play', index: 1, complete: false },
+      { description: 'eat', index: 2, complete: true },
+    ]));
+    clearAllFunction();
+    expect(global.localStorage.setItem).toBeCalledWith(
+      'tasks',
+      JSON.stringify(tasksArray),
+    );
+  });
+
+  test('Update a task to completed', () => {
+    const localStorageMock = {
+      getItem: jest.fn(),
+      setItem: jest.fn(),
+      clear: jest.fn(),
+    };
+    global.localStorage = localStorageMock;
+    const array = [
+      { description: 'Play', completed: true, index: 1 },
+      { description: 'eat', completed: false, index: 2 },
+    ];
+    CompletedTask(array, 2);
+    expect(global.localStorage.setItem).toBeCalledWith(
+      'tasks',
+      JSON.stringify([
+        { description: 'Play', completed: true, index: 1 },
+        { description: 'eat', completed: true, index: 2 },
+      ]),
+    );
+  });
+});
